7ef05819f8dec5fa02693c13631aac55d8e2cf75
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2023-12-22 17:56:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const API_URL = 'https://5b987o7oal.execute-api.us-east-2.amazonaws.com/Prod/';
 
-fetch(API_URL, { mode: 'no-cors' })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('API request failed');
-    }
-    return response.text(); // Get the response text
-  })
-  .then(text => {
-    console.log('API response string:', text);
-    // Display the string on your webpage
+fetch(API_URL)
+  .then(response => response.json())
+  .then(data => {
+    const apiResponseDiv = document.getElementById('api-response');
+    apiResponseDiv.textContent = data.body; // Display the API response text
+    console.log('API response:', data); // Log the API response to the console
   })
   .catch(error => {
     console.error('Error fetching API data:', error);
+    apiResponseDiv.textContent = 'Error fetching data from API.'; // Display error message
   });
